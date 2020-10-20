9edb34569e86d9452c9b8e861f6e3f004c88c4b7
zkadam <adam.zakar@student.careeria.fi>
2020-10-20 16:21:51 +0300

    logins get lisätty
    logins get lisätty

--- src/Navigaatio.js
+++ src/Navigaatio.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import NWCustomerFetch from './NWCustomerFetch'
+import NWLoginsFetch from './NWLoginsFetch'
 
 
 
@@ -16,12 +17,16 @@ class Navigaatio extends Component {
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <ul className="navbar-nav mr-auto">
                     <li><Link to={'/NWCustomerFetch'} className="nav-link">NWCustomerFetch</Link></li>
+                    <li><Link to={'/NWLoginsFetch'} className="nav-link">NWLoginsFetch</Link></li>
+
                 </ul>    
                 </nav>
                 <hr/>
 
         <Switch>
             <Route path='/NWCustomerFetch' component={NWCustomerFetch}/>
+            <Route path='/NWLoginsFetch' component={NWLoginsFetch}/>
+
         </Switch>
             </div>
         </Router>
